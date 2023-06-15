01ac8c92db71806a4c604b7cc40110cb3e87fbc1
yuye <18838280615@163.com>
2023-06-15 19:17:20 +0800

    sh脚本更新

--- src/shell脚本/切换本地host/shellChangeHost.sh
+++ src/shell脚本/切换本地host/shellChangeHost.sh
@@ -5,12 +5,14 @@ echo "请输入一个选项："
 echo "1、 switch（会覆盖本地host）"
 echo "2、 append（在本地host后追加）"
 read -p "请输入：" choiceType
-
 read -p "请输入要切换的 Host 类型（test 或 staging）：" hostType
 
+# 确定当前脚本文件夹下的文件
+scriptDir=$(dirname "$0")
 # 测试写入当前文件的newtest里面
-yyhosts=./newtest
+yyhosts=$scriptDir/newtest
 # yyhosts=/etc/hosts
+
 
 loadingLog() {
     echo "操作系统：$1  ⏰ \n切换模式：$choiceType 🚀 \n切换到$hostType 更改中.....🍖"
@@ -18,15 +20,17 @@ loadingLog() {
 
 case "$hostType" in
   test)
-    inputFile="./test"
+    inputFile=$scriptDir/test
     outputFile=$yyhosts
     ;;
   staging)
-    inputFile="./staging"
+    inputFile=$scriptDir/staging
     outputFile=$yyhosts
     ;;
   *)
 esac
+echo "文件夹下文件inputFile：$inputFile"
+echo "文件夹下文件outputFile：$outputFile"
 
 # 切换host（会覆盖）
 switchHost() {
@@ -34,12 +38,12 @@ switchHost() {
     then
         if [ "$hostType" == "test" ]
         then
-            sudo cp test $yyhosts
-            echo "已切换为测试环境 Host 🎉🎉🎉"
+            sudo cp $inputFile $outputFile || { echo >&2 "文件切换失败！❌❌❌"; exit 1; }
+            echo "已切换为test环境 🎉🎉🎉"
         elif [ "$hostType" == "staging" ]
         then
-            sudo cp staging $yyhosts
-            echo "已切换为测试环境 staging 🎉🎉🎉"
+            sudo cp $inputFile $outputFile || { echo >&2 "文件切换失败！❌❌❌"; exit 1; }
+            echo "已切换为staging环境 🎉🎉🎉"
         else
             echo "输入：$hostType 为未知的Host环境！ ❌❌❌"
         fi
@@ -49,7 +53,7 @@ switchHost() {
 contentAppend() {
     content=$(cat "$inputFile")
     # 追加内容到输出文件
-    sudo sh -c "echo \"$content\" >> "$outputFile""
+    sudo sh -c "echo \"$content\" >> "$outputFile"" || { echo >&2 "文件写入失败！❌❌❌"; exit 1; }
     echo "$1 Host已经追加 🎉🎉🎉"
 }
 # 追加host（不会覆盖）
