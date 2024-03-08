759a385c512dbe87a19dd868d5e6b09577b4c834
yuye <659672626@qq.com>
2024-03-08 23:14:34 +0800

    代码提交

---
+++ src/手写JS大全/flatter.ts
@@ -0,0 +1,30 @@
+// 数组扁平化
+// console.log(flatter([1, 2, [1, [2, 3, [4, 5, [6]]]]]));
+
+const ary = [1, 2, [1, [2, 3, [4, 5, [6]]]]];
+
+const flatter = (ary: any[]) => {
+  while (
+    ary.some((item: any) => {
+      console.log("isary---", item);
+      return Array.isArray(item);
+    })
+  ) {
+    console.log("---", ary);
+    ary = [].concat(...ary);
+    console.log(ary);
+  }
+  // console.log(ary);
+};
+
+const flatterReduce = (ary: any[]) => {
+  const flatterAry: any = ary.reduce((pre, cur) => {
+    // console.log("===", pre);
+    return Array.isArray(cur) ? [...pre, ...flatterReduce(cur)] : [...pre, cur];
+  }, []);
+  console.log(ary);
+  return flatterAry;
+};
+
+console.log(flatterReduce(ary));
+// flatter(ary);
